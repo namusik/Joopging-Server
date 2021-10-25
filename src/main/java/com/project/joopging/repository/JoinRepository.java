de2f12533a427fbd20f5d8af178cf87314a5b198
namusik <wsnam0418@gmail.com>
2021-10-25 21:49:45 +0900

    join 기능 만들기

--- src/main/java/com/project/joopging/repository/JoinRepository.java
+++ src/main/java/com/project/joopging/repository/JoinRepository.java
@@ -1,7 +1,15 @@
 package com.project.joopging.repository;
 
 import com.project.joopging.model.Join;
+import com.project.joopging.model.Post;
+import com.project.joopging.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JoinRepository extends JpaRepository<Join, Long> {
+
+    void deleteByUserJoinAndPostJoin(User user, Post post);
+
+    Optional<Join> findByUserJoinAndPostJoin(User user, Post post);
 }
