fb2bed792e3d493464690415d6dedd9e8e0402d9
KimSeonkyum <rlatjsrua02@naver.com>
2021-10-25 15:49:10 +0900

    user 설정 완료

--- src/main/java/com/project/joopging/JoopgingApplication.java
+++ src/main/java/com/project/joopging/JoopgingApplication.java
@@ -2,8 +2,12 @@ package com.project.joopging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ServletComponentScan
 public class JoopgingApplication {
 
     public static void main(String[] args) {
