34772b1d9ff7a0884b628bd75130955ab8e3e8fe
kancho06 <wotjd5792@gmail.com>
2021-11-19 01:11:07 +0900

    11/19 sms crewHead 메세지
    11/19 sms crewHead 메세지

--- src/main/java/com/project/joopging/service/SmsService.java
+++ src/main/java/com/project/joopging/service/SmsService.java
@@ -11,7 +11,7 @@ import com.project.joopging.util.coolsms.GroupModel;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import retrofit2.Call;
@@ -23,13 +23,13 @@ import java.time.format.TextStyle;
 import java.util.List;
 import java.util.Locale;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class SmsService {
 
     private final PostRepository postRepository;
 
-    public void sendSms(JsonArray toList, String postTile) {
+    public void sendSms(JsonArray toList, String message) {
 
         JsonObject params = new JsonObject();
         JsonArray messages = new JsonArray();
@@ -40,8 +40,7 @@ public class SmsService {
 
         msg.add("to", toList);
         msg.addProperty("from", "01099403102");
-        msg.addProperty("text",
-                "안녕하세요 줍깅입니다. 신청하신"+ "[ " + postTile + " ]" +"모임의 모임날짜가 하루전입니다.");
+        msg.addProperty("text", message);
         messages.add(msg);
 
         params.add("messages", messages);
@@ -80,36 +79,83 @@ public class SmsService {
         List<Post> postList = postRepository.findAll();
         JsonArray toList = new JsonArray();
         for (Post post : postList) {
-            String nowPlusOneDay = getLocalDateTimeNowToString(LocalDateTime.now().plusDays(1));
-            String runningDate = getRunningDateToString(post);
+            String nowPlusOneDay = getLocalDateTimeNowToStringDay(LocalDateTime.now().plusDays(1));
+            String runningDate = getRunningDateToStringDay(post);
           if (nowPlusOneDay.equals(runningDate)) {
               List<Crew> crewList = post.getCrew();
               String postTitle = post.getTitle();
+              String message = "안녕하세요 줍깅입니다. 신청하신"+ " [" + postTitle + "] " +"모임의 모임날짜가 하루전입니다.";
               for (Crew crew : crewList) {
                   User user = crew.getUserJoin();
                   String userNumber = user.getNumber();
                   toList.add(userNumber);
               }
-              sendSms(toList, postTitle);
+              sendSms(toList, message);
           }
         }
     }
 
-    private String getRunningDateToString(Post post) {
+    //스케쥴러 1분마다 체크
+    //CrewHead 에게 출석체크 url 알럿문자메세지
+//    @Scheduled(cron ="0 0/1 * * *" )
+    public void sendAttendanceCheckAlertToCrewHead() {
+        List<Post> postList = postRepository.findAll();
+        JsonArray toList = new JsonArray();
+        for (Post post : postList) {
+            String runningDate = getRunningDateToStringDay(post);
+            String now = getLocalDateTimeNowToStringMinute(LocalDateTime.now());
+            if (now.equals(runningDate)) {
+                String number = post.getWriter().getNumber();
+                String message = "안녕하세요 줍깅입니다. 다 모이셨나요? 출석체크를 해주세요!" +
+                        "출석체크는 앞으로 유저간의 신뢰도를 측정하는데 도움이 됩니다!" +
+                        "출석체크 url";
+                toList.add(number);
+                sendSms(toList,message);
+            }
+
+        }
+    }
+
+
+
+
+    //요일까지만 비교 (runningDate 1일전 알럿용)
+    private String getRunningDateToStringDay(Post post) {
         LocalDateTime runningDate = post.getRunningDate();
         String day = runningDate.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN);
         String date = String.valueOf(runningDate);
 //        System.out.println("date = " + date);
         String[] ts = date.split("T");
-        return ts[0] + " ("+day+") ";
+        return ts[0] + " ("+ day +") ";
     }
 
-    private String getLocalDateTimeNowToString(LocalDateTime localDateTime) {
+    //요일까지만 비교 (runningDate 1일전 알럿용)
+    private String getLocalDateTimeNowToStringDay(LocalDateTime localDateTime) {
         String day = localDateTime.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN);
         String date = String.valueOf(localDateTime);
 //        System.out.println("date = " + date);
         String[] ts = date.split("T");
-        return ts[0] + " ("+day+") ";
+        return ts[0] + " (" + day + ") ";
+    }
+    // 분까지 비교 (출석 체크 알럿용)
+    private String getRunningDateToStringMinute(Post post) {
+        LocalDateTime runningDate = post.getRunningDate();
+        String day = runningDate.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN);
+        String date = String.valueOf(runningDate);
+//        System.out.println("date = " + date);
+        String[] ts = date.split("T");
+        String[] ms = ts[1].split(":");
+        return ts[0] + " ("+ day +") " + ms[0] + ms[1];
+    }
+
+    // 분까지 비교 (출석 체크 알럿용)
+    private String getLocalDateTimeNowToStringMinute(LocalDateTime localDateTime) {
+        String day = localDateTime.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN);
+        String date = String.valueOf(localDateTime);
+//        System.out.println("date = " + date);
+        String[] ts = date.split("T");
+        String[] ms = ts[1].split(":");
+        return ts[0] + " (" + day + ") " + ms[0] + ms[1];
     }
 }
 
