8f36e858d830d514bda1c2bdc0322844ead80e79
namusik <79183229+namusik@users.noreply.github.com>
2022-07-26 16:09:44 +0900

    hello controller 추가

--- src/main/java/com/project/joopging/controller/ProfileController.java
+++ src/main/java/com/project/joopging/controller/ProfileController.java
@@ -11,17 +11,22 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class ProfileController {
     private final Environment env;
-    
+
     //현재 어떤 포트로 돌아가는지 확인용
     @GetMapping("/profile")
     public String profile() {
         //현재 동작중인 프로파일의 이름을 반환
         return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
-    
+
     //version 확인용
     @GetMapping("/version")
-        public String checkVersion() {
-            return "ver4";
-        }
+    public String checkVersion() {
+        return "ver4";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "jenkins build Success";
+    }
 }
